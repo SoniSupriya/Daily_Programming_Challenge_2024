53816aa24093b6bfe90b3a9392cfbb36e6cf1457
SoniSupriya <147236057+SoniSupriya@users.noreply.github.com>
2024-09-10 12:48:04 +0530

    Add files via upload

---
+++ find_missing_num_02.py
@@ -0,0 +1,12 @@
+def missingNumber(nums):
+    n = 1
+    for i in range(1,len(nums)):
+        if(n in nums):
+            n =n+1
+        elif((n not in nums)and(1<=n<len(nums))):
+            return n
+            break
+    else:
+        return len(nums)
+nums = [1]
+print(missingNumber(nums))
